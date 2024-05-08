8f653b1b937c34c288eb77e5f86b8f32bc1642f6
Pushpa Leela Kumari <108827336+pushpaleelakumari@users.noreply.github.com>
2024-05-08 21:46:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 //your JS code here. If required.
+let outputTable = document.getElementById("output")
+outputTable.innerHTML = "<b>Loading...</b>"
+
 const promise1 = () => {
-	return new Promise((resolve,reject)=>{
-		setTimeout(()=>{
-			resolve("promise1 resolved")
-		},3000)
-	})
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("promise1 resolved")
+        }, 3000)
+    })
 }
 
 const promise2 = () => {
-	return new Promise((resolve,reject)=>{
-		setTimeout(()=>{
-			resolve("promise2 resolved")
-		},1000)
-	})
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("promise2 resolved")
+        }, 1000)
+    })
 }
 
 const promise3 = () => {
-	return new Promise((resolve,reject)=>{
-		setTimeout(()=>{
-			resolve("promise3 resolved")
-		},2000)
-	})
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("promise3 resolved")
+        }, 2000)
+    })
 }
 
-Promise.all([promise1(),promise2(),promise3()]).then((result)=>{
-	outputTable.innerHTML = `
-    <tr><td>${results[0].name}</td><td>1 sec</td></tr>
-    <tr><td>${results[1].name}</td><td>2 sec</td></tr>
-    <tr><td>${results[2].name}</td><td>3 sec</td></tr>
-  `;
-})
+const resolveAll = () => {
+    Promise.all([promise1(), promise2(), promise3()]).then((result) => {
+        outputTable.innerHTML = ""
+        result.map((promis, index) => {
+            outputTable.innerHTML += `
+            <tr><td>${promis}</td><td>${index + 1} sec</td></tr>
+            `
+        })
+    })
+}
+
+resolveAll()
