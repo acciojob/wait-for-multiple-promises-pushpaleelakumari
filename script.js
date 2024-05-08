b94e0591f3fd9516fd6e5b8e25aac0040c93d21f
Pushpa Leela Kumari <108827336+pushpaleelakumari@users.noreply.github.com>
2024-05-08 21:15:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+const promise1 = () => {
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("promise1 resolved")
+		},3000)
+	})
+}
+
+const promise2 = () => {
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("promise2 resolved")
+		},1000)
+	})
+}
+
+const promise3 = () => {
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			resolve("promise3 resolved")
+		},2000)
+	})
+}
+
+Promise.all([promise1(),promise2(),promise3()]).then((result)=>{
+	outputTable.innerHTML = `
+    <tr><td>${results[0].name}</td><td>1 sec</td></tr>
+    <tr><td>${results[1].name}</td><td>2 sec</td></tr>
+    <tr><td>${results[2].name}</td><td>3 sec</td></tr>
+  `;
+})
